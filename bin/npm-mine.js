bf35312f5b24709a18ef09f03859d10f9a441c14
Alex Puschinsky <alexpoo@gmail.com>
2015-10-23 16:50:58 +0300

    Refactored bin/npm-mine

--- bin/npm-mine.js
+++ bin/npm-mine.js
@@ -1,34 +1,41 @@
 #! /usr/bin/env node
 parseArgs = require("minimist")
 _ = require("lodash")
-
 NpmMine = require("../lib/index")
+function validateAction(action){
+  return ["metadata", "download", "count"].indexOf(action) >= 0
+}
+
+function validateArgs(action, args){
+  switch(action){
+    case "download": 
+      if(args.path === undefined){
+        console.log("Missing argument: path");
+        return false;
+      }
+  }
+
+  return true;
+}
+
+function printUsage(){
+  var usage = "usage: npm-mine <command> [--threshold=threshold] [--path=path]\n\n"
+  usage += "commands:\n"
+  usage += "\tmetadata \t mines npms metadata and downloads from npm's repository\n"
+  usage += "\tdownload \t downloads all npms with more than [threshold] downloads last month\n"
+  usage += "\tcount \t\t counts all npms with more than [threshold] downloads last month\n"
+
+  console.log(usage);
+}
 
 var args = parseArgs(process.argv.slice(3));
 var action = process.argv[2];
 
-var legal = true;
+args = _.defaults(args, {
+  threshold: 500,
+})
 
-switch(action){
-  case "count":
-    args = _.defaults(args, {
-      threshold: 500,
-    })
-
-    break;
-    
-  case "download":
-    args = _.defaults(args, {
-      threshold: 500,
-    })
-
-    if(args.path === undefined){
-      console.log("Missing argument: path");     
-      legal = false
-    }
-
-    break;
-}
-
-if(legal)
+if(validateAction(action) && validateArgs(action, args))
   NpmMine[action].call(null, args);
+else
+  printUsage();
